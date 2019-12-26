32b0207e009ed1b8c6a401a25fb50b752114813b
negue <eugen.bolz@gmail.com>
2019-12-26 23:16:01 +0100

    add polyfill and buffer

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -61,4 +61,10 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+// Blockstack polyfills
+(window as any).global = window;
+global.Buffer = global.Buffer || require('buffer').Buffer;
+(window as any).process = {
+  version: ''
+};
 
